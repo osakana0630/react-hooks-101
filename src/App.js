bd3322cf3209961a5d51b115d7568c7b21f69919
osakana0630 <osakana0630@gmail.com>
2020-05-27 19:39:12 +0900

    implement object state

--- src/App.js
+++ src/App.js
@@ -2,29 +2,20 @@ import React, {useState} from 'react';
 
 
 const App = (props) => {
-    // //初期値の設定
-    // const initialStates = {
-    //     name: "",
-    //     price: 1000
-    // };
+    //useStateの引数にオブジェクトを渡すことできる。抽象的な状態を持つことができる。
+    const [state, setState] = useState(props);
+    const {name, price} = state;
 
-    const [name, setName] = useState(props.name);
-    const [price, setPrice] = useState(props.price);
-
-    const reset = () => {
-        setPrice(props.price);
-        setName(props.name)
-    };
 
 
     return (
         <React.Fragment>
             <p>現在の{name}は、{price}円です</p>
-            <button onClick={() => setPrice(price + 1)}>+1</button>
-            <button onClick={() => setPrice(price - 1)}>-1</button>
-            <button onClick={reset}>Reset</button>
-            {/*e.target.valueでonChaneが発生した時にinputに入力させている値を拾うことができる*/}
-            <input value={name} onChange={e => setName(e.target.value)}/>
+            <button onClick={() => setState({...state, price: price + 1})}>+1</button>
+            <button onClick={() => setState({...state, price: price - 1})}>-1</button>
+            <button onClick={() => setState(props) }>Reset</button>
+            {/*setStateの引数にはオブジェクトを受け取る*/}
+            <input value={name} onChange={e => setState({...state, name: e.target.value})}/>
 
 
         </React.Fragment>
