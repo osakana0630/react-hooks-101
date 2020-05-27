805ee1daf9719bfbbab8202390e0369b099bbfd5
osakana0630 <osakana0630@gmail.com>
2020-05-27 23:42:45 +0900

    implement event creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useReducer, useState} from 'react';
+import reducer from "../reducers"
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const App = () => {
+    const [state, dispatch] = useReducer(reducer, []);
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+
+
+    const addEvent = e => {
+        //画面のリロードを防ぐ
+        e.preventDefault();
+        console.log({title, body});
+        dispatch({
+            type: "CREATE_EVENT",
+            title,
+            body
+        });
+        setTitle("");
+        setBody("");
+    };
+
+
+    console.log({state});
+
     return (
         <div className="container-fluid">
 
@@ -12,32 +35,32 @@ const App = () => {
             <form>
                 <div className="form-group">
                     <label htmlFor="formEventTitle">タイトル</label>
-                    <input className="form-control" id="formEventTitle"/>
+                    <input className="form-control" id="formEventTitle" value={title}
+                           onChange={e => setTitle(e.target.value)}/>
                 </div>
 
                 <div className="form-group">
                     <label htmlFor="formEventBody">ボディ</label>
-                    <textarea className="form-control" id="formEventBody"/>
+                    <textarea className="form-control" id="formEventBody" value={body} onChange={e => {
+                        setBody(e.target.value)
+                    }}/>
                 </div>
-                <button className="btn btn-primary">イベントを作成する</button>
+                <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
                 <button className="btn btn-danger">全てのイベントを削除する</button>
             </form>
-          <h4>イベント一覧</h4>
-          <table className="table table-hover">
-            <thead>
-            <tr>
-              <th>ID</th>
-              <th>タイトル</th>
-              <th>ボディ</th>
-            </tr>
-            </thead>
-            <tbody>
-            </tbody>
+            <h4>イベント一覧</h4>
+            <table className="table table-hover">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>タイトル</th>
+                    <th>ボディ</th>
+                </tr>
+                </thead>
+                <tbody>
+                </tbody>
 
-          </table>
-
-
-
+            </table>
 
 
         </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -18,8 +18,8 @@ const events = (state = [], action) => {
         case "CREATE_EVENT" :
             const event = { title: action.title, body: action.body};
             const length = state.length;
-            const id = (length === 0) ? id = 1 : id = state[length - 1]. id + 1;
-            return [...state, {id: id, ...event}]
+            const id = (length === 0) ? 1 : state[length - 1].id + 1;
+            return [...state, {id, ...event}];
             return state;
         case "DELETE_EVENT" :
             return state;
